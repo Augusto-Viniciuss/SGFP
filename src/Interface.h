d25deb27b9c14ee87ed7293664ed3ae62fe6254d
Augusto Vinicius <augustoviniciud@gmail.com>
2022-05-21 01:02:31 -0300

    Update Interface.h

--- src/Interface.h
+++ src/Interface.h
@@ -10,7 +10,7 @@
 class Interface {
     public:
         int menu();
-        Funcionario* cadastraFuncionario();
+        Funcionario* lerAtributosFuncionario();
         int lerCodigoParaModificarFuncionario();
         int lerOpcaoParaModificarFuncionario();
         template<typename atributo> atributo novoAtributoParaModificarFuncionario();
