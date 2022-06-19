f45baeb2e48fc21f1997c908908e6d150536b479
Renata <renataaandrade02@gmail.com>
2022-06-19 20:04:37 -0300

    comentei

--- src/Gerente.h
+++ src/Gerente.h
@@ -6,13 +6,18 @@
 
 class Gerente : public Funcionario{
     public:
+        /* Construtor padrão sem argumentos */
         Gerente();
+        /* Construtor que recebe os atributos do Gerente */
         Gerente(int codigo, std::string nome, std::string CPF, int idade, std::string endereco, int numeroResidencia, std::string telefone, int *data, int designacao, std::string areaSupervisao);
         
+        /* Get */
         std::string getAreaSupervisao();
         
+        /* Set */
         void setAreaSupervisao(std::string areaSupervisao);
         
+        /* Método sobrescrito para calcular o salario mensal */
         void calcularSalarioMensal(int mes);
 
     private:
