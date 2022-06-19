c1f2cd2d0dbda1ab311243f9726d329c92ad3dc6
Renata <renataaandrade02@gmail.com>
2022-06-19 19:01:29 -0300

    comentei

--- src/OpcaoInvalidaException.h
+++ src/OpcaoInvalidaException.h
@@ -3,9 +3,17 @@
 
 #include <exception>
 
+/*
+    Exception personalizado criado para reconhecer e
+    informar ao usuário caso haja uma digitação incorreta
+    ou uma opção inválida
+*/
+
 class OpcaoInvalidaException : public std::exception{
     public:
+        /* Construtor que recebe um array de char da mensagem de erro */
         OpcaoInvalidaException(const char* e);
+        /* Método para exibir a mensagem de erro */
         virtual const char* what();
     protected:
         char mensagem[100];
--- src/TelefoneException.cpp
+++ src/TelefoneException.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 
 TelefoneException::TelefoneException(const char* e){
+     //Fazendo o uso da função strcpy para passar a mensagem
+    //recebida para a variável char mensagem[100]
     strcpy(mensagem, e);
 }
 
