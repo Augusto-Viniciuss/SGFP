0e43e3eed9cd4718277097e5eb988f0234fd3dfe
Renata <renataaandrade02@gmail.com>
2022-06-21 15:26:40 -0300

    Comentando os exception

--- src/FuncionarioNaoEstaCadastradoExcept.h
+++ src/FuncionarioNaoEstaCadastradoExcept.h
@@ -3,15 +3,27 @@
 
 #include <exception>
 
-/*
-    Exception personalizado criado para reconhecer e
-    informar ao usuário na hora de utilizar algum método
-    que exija a existência de um funcionário
+/**
+* Exception personalizado criado para reconhecer e
+* informar ao usuário na hora de utilizar algum método
+* que exija a existência de um funcionário
 */
 
 class FuncionarioNaoEstaCadastradoExcept : public std::exception{
     public:
+        /**
+        * Construtor
+        * 
+        * Inicializa o atributo mensagem[] com a mensagem passada pela chamada do erro.
+        * \param char*-e array de char referente a mensagem de erro
+        */
         FuncionarioNaoEstaCadastradoExcept(const char* e);
+
+        /**
+        * what( )
+        * 
+        * Método para a exibição da mensagem de erro
+        */
         virtual const char* what();
     protected:
         char mensagem[100];
